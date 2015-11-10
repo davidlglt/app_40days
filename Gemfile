bf40362b9fc1ce5a33bfb3b35ece9992ad598fca
David <davi.legault@gmail.com>
2015-11-10 15:19:57 +0100

    uplaod picture in course

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'aws-sdk', '< 2.0'
+gem 'paperclip'
 
 #Admin Backend
 gem 'activeadmin', github: 'activeadmin'
--- app/admin/course.rb
+++ app/admin/course.rb
@@ -1,6 +1,25 @@
 ActiveAdmin.register Course do
-  permit_params :name, :content, :price, :video_teaser
-end
+  permit_params :name, :content, :price, :video_teaser, :image
 
-
-
+  show do |t|
+    attributes_table do
+      row :name
+      row :content
+      row :price
+      row :video_teaser
+      row :image do
+        course.image? ? image_tag(course.image.url, height: '100') : content_tag(:span, "Vous n'avez pas ajouté d'image")
+        end
+      end
+    end
+    form :html => { :enctype => "multipart/form-data" } do |f|
+      f.inputs do
+        f.input :name
+        f.input :content
+        f.input :price
+        f.input :video_teaser
+        f.input :image, hint: f.course.image? ? image_tag(course.image.url, height: '100') : content_tag(:span, "uplaod au format png, jpeg, gif")
+    end
+    f.actions
+    end
+  end
