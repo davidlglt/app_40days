303e18acc84a801a6ef024cd54418369c5d25788
Ricardo Remy <ricardo.remy@outlook.com>
2015-11-10 20:28:12 +0100

    In progress

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,4 +8,9 @@ class Course < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "680x300>", thumb: "170x75>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  def average_rating
+    reviews.blank? ? 0 : reviews.average(:star).round(2)
+  end
+
 end
