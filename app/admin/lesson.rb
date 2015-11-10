b2bfe03f2f5071b242b9442314d4354d56edb380
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-10 16:46:58 +0100

    install of gem for drag and drop

--- app/admin/lesson.rb
+++ app/admin/lesson.rb
@@ -1,4 +1,23 @@
 ActiveAdmin.register Lesson do
   permit_params :name, :note, :video, :header, :tag, :course_id
+
+  sortable tree: false,
+                  sorting_attribute: :tag
+
+  index :as => :sortable do
+    label :name
+
+    actions
+  end
+
+  index do
+    selectable_colum
+    colum :header
+    colum :title
+    colum :tag
+    colum :course
+
+    actions
+  end
 end
 
