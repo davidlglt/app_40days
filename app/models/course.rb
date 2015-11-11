098df7c324d1a31e6c54dd3ffb8633ec5ab52d6e
David <davi.legault@gmail.com>
2015-11-11 18:51:08 +0100

    Front sur home page

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,6 @@ class Course < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 500}
   validates :price, presence: true, numericality: { only_integer: true}
 
-  has_attached_file :image, styles: { medium: "680x300>", thumb: "170x75>" }
+  has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "170x75#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
