3a399ac54c64d279f9b3285f1e2f1a1183ceab76
David <davi.legault@gmail.com>
2015-11-12 18:50:00 +0100

    courses show and lessons show in progress

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -3,6 +3,10 @@ class LessonController < ApplicationController
     course = Course.find(params[:course_id])
     @lessons = course.lessons.order(:tag)
     @lesson = @lessons.find(params[:id])
+
+    @next_lesson = @lesson.next
+    @prev_lesson = @lesson.prev
+
     authorize @lesson
   end
 end
