4c4f00d7225c0d6c7af8b063891835bf47d5c6d1
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-13 14:23:30 +0100

    modification de la navbar et footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,14 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+.caret {
+  display: inline-block;
+  width: 0;
+  height: 0;
+  margin-left: 2px;
+  vertical-align: middle;
+  border-top: 4px solid;
+  border-right: 4px solid transparent;
+  border-left: 4px solid transparent;
+}
