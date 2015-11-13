bcb15921ddabe8e867781cb69b8a212461b17230
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-13 15:46:10 +0100

    solved carret

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,13 +3,4 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-.caret {
-  display: inline-block;
-  width: 0;
-  height: 0;
-  margin-left: 2px;
-  vertical-align: middle;
-  border-top: 4px solid;
-  border-right: 4px solid transparent;
-  border-left: 4px solid transparent;
-}
+
