c6273aadc1ff92095452343ad96dcb834559e1bb
Ricardo Remy <ricardo.remy@outlook.com>
2015-11-18 17:53:29 +0100

    Front payment

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ Lesson.create!(name: "Penalty", video: "iovihdufe0", tag: 3, course_id: 1, posit
 Lesson.create!(name: "Arrêts et parades", tag: 3, video: "7j4zix8pxq", course_id: 1, position: 10)
 
 
-Course.create!(name: 'Comprendre GOT en Anglais', duration: 40, total_words: 950, price: 59, video_teaser: "7j4zix8pxq", image: File.open(Rails.root.join('db/seeds/images/GOT.png') ) )
+Course.create!(name: 'Comprendre GOT en Anglais', duration: 40, total_words: 950, price: 59, video_teaser: "7j4zix8pxq", image: File.open(Rails.root.join('db/seeds/images/got.png') ) )
 
 Course.create!(name: 'Lire et comprendre "The old man and the sea"', duration: 40, total_words: 800, price: 49, video_teaser: "7j4zix8pxq", image: File.open(Rails.root.join('db/seeds/images/old_man.jpg') ) )
 
