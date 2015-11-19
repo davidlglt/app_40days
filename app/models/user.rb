0caaae6efe6ca059690db24aed3901cf2511305b
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-19 11:33:41 +0100

    modification au niveau de la connection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+  devise :database_authenticatable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   has_many :reviews
   has_many :subscriptions, dependent: :destroy
@@ -16,7 +12,6 @@ class User < ActiveRecord::Base
 
 
 
-  devise :omniauthable, omniauth_providers: [:facebook]
 
   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
