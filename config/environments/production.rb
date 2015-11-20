206bdfd4abbc8524b46c4d762592c1378dd99bb5
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-20 10:46:49 +0100

    modification footer, mise en place des metatags et favicon 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  DEFAULT_META = YAML.load_file(Rails.root.join('config/meta.yml'))
   config.action_mailer.default_url_options = { host: "https://app40days.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
